97acb9c27bc2fd34f7bde29bc2b0be38c8254f9c
Asad Ali <softwareengineer.asadali@gmail.com>
2025-05-17 21:53:41 +0500

    TicTacToe

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Tictactoe from './Components/tictactoe/tictactoe.jsx'
+
+function App() {
+  return (
+    <div>
+      <Tictactoe />
+    </div>
+  )
+}
+
+export default App
